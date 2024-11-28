dd747999fd5a77b2a12e0d205c9ab37e48d0924b
Malika <elabiadmalika64@gmail.com>
2024-11-28 19:18:11 +0100

    ajout de fonction afficher les joueurs #5

--- js/players.js
+++ js/players.js
@@ -41,3 +41,27 @@ if (!localStorage.getItem("players")) {
         .catch(error => console.error("Erreur :", error));
 }
 const players = JSON.parse(localStorage.getItem("players"));
+
+    function afficherJoueur(){
+        joueurContainer.innerHTML = ``;
+        players.forEach(player => {
+            joueurContainer.innerHTML += `
+                <div data-nat="${player.nationality}" data-club="${player.club}" id="playersCarte" class=" relative">
+                    <img id="LW" class=" w-60" src="../images/card.png" alt="LW">
+                    <img class="photo absolute left-[29%] top-[15%] w-[120px] h-[120px]" src="${player.photo}">
+                    <span class="rating absolute left-[24%] top-[22%] text-[22px] text-customText ">${player.rating}</span>
+                    <P class="position absolute left-[24%] top-[32%] text-customText ">${player.position}</p>
+                    <img class="logo absolute left-[26%] top-[40%] w-[16px] h-[16px]" src="${player.logo}">
+                    <p class=" name whitespace-nowrap absolute left-[28%] top-[52%] font-extralight text-customText">${player.name}</p>
+                    <img class="flag absolute left-[65%]  top-[53%] font-extralight w-[18] h-[18px] " src="${player.flag}" alt="">
+                    <p class="pace diving text-[6px] whitespace-nowrap absolute left-[28%] top-[64%]  font-extralight text-customText">${player.position == 'GK' ? player.diving : player.pace} ${player.position == 'GK' ? 'DIV' : 'PAC'} </p>
+                    <p class="shooting handling text-[6px] whitespace-nowrap absolute left-[28%] top-[69%]  font-extralight text-customText">${player.position == 'GK' ? player.handling : player.shooting} ${player.position == 'GK' ? 'HAN' : 'SHO'} </p>
+                    <p class="passing kicking text-[6px] whitespace-nowrap absolute left-[28%] top-[75%]  font-extralight text-customText">${player.position == 'GK' ? player.kicking : player.passing} ${player.position == 'GK' ? 'KICK' : 'PAS'} </p>
+                    <p class="dribbling reflexes dribbling text-[6px] whitespace-nowrap absolute left-[57%] top-[64%]  font-extralight text-customText">${player.position == 'GK' ? player.reflexes : player.dribbling} ${player.position == 'GK' ? 'REF' : 'DRI'} </p>
+                    <p class="speed defending text-[6px] whitespace-nowrap absolute left-[57%] top-[69%]  font-extralight text-customText">${player.position == 'GK' ? player.speed : player.defending} ${player.position == 'GK' ? 'SPE' : 'DEF'} </p>
+                    <p class="positioning physical text-[6px] whitespace-nowrap absolute left-[57%] top-[75%]  font-extralight text-customText">${player.position == 'GK' ? player.positioning : player.physical} ${player.position == 'GK' ? 'POS' : 'PHY'} </p>
+                </div> 
+            `  
+
+        });
+    }
