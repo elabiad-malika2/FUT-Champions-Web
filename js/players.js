94db80d23b6c59a66da215ab8c42fc6bcfc3d49b
Malika <elabiadmalika64@gmail.com>
2024-11-28 19:04:53 +0100

    initialiser tous les constants pour acceder avec l'id #5

---
+++ js/players.js
@@ -0,0 +1,25 @@
+const joueurContainer = document.getElementById("joueurs-carte");
+const positionPlayer = document.getElementById("PositionPlayer");
+const statPlayers = document.getElementById("playersSta");
+
+let divForm = null;
+let hanForm = null;
+let kickForm = null ;
+let refForm = null ;
+let speForm = null ;
+let posForm = null ;
+
+let phyForm = null ;
+let defForm = null ;
+let driForm = null ;
+let pasForm = null ;
+let shoForm = null ;
+let pacForm = null ;
+
+const nomForm = document.getElementById("name");
+const photoForm = document.getElementById("photo");
+const nationalityForm = document.getElementById("nationality");
+const flagForm = document.getElementById("flag");
+const clubForm = document.getElementById("club");
+const logoForm = document.getElementById("logo");
+const ratingForm = document.getElementById("rating");
