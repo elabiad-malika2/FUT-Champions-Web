48429c785eaf9c21eeb1aab56939fa0f15350d73
Malika <elabiadmalika64@gmail.com>
2024-12-01 13:10:33 +0100

    initailaiser le divs et les appeler  #1

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let allCards = document.querySelectorAll(".card");
+let modele = document.getElementById("popupModal");
+let playersContent = document.getElementById("playersContainer");
+let indexPlayer = null ;
