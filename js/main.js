f3fc0b43903d71e6fccc978f02e949178bc1c1e8
Malika <elabiadmalika64@gmail.com>
2024-12-01 13:18:05 +0100

    Regeler le burger menu quand je clique il m'affiche les pages#1

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ let allCards = document.querySelectorAll(".card");
 let modele = document.getElementById("popupModal");
 let playersContent = document.getElementById("playersContainer");
 let indexPlayer = null ;
+
+// For menu :
+const menuButton = document.getElementById("mobile-menu-button");
+        const mobileMenu = document.getElementById("mobile-menu");
+
+    menuButton.addEventListener("click", () => {
+            mobileMenu.classList.toggle("hidden");
+    });
